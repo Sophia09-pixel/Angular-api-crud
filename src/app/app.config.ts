44b653a00bffd41503b57c5313ca30357937960b
Sophia <sophia.dev0903@gmail.com>
2025-05-23 20:16:48 -0300

    feat: projeto finalizado

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,26 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay())]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(),
+  ],
+  //npm i json-server
+  //Abra o package.json e dentro de scripts insira:
+  //"server": "json-server --watch db.json
+  //Abra o arquivo app.config.ts para configurar o provedor Http
+  //import { provideHttpClient } from '@angular/common/http';
+  //export const appConfig: ApplicationConfig = {
+  //providers: [provideRouter(routes), provideClientHydration(), provideHttpClient()
+  //};
+  //npm run server
 };
